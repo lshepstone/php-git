0a41a40c52345b298bdbc78e16c0aa76cb3472e0
Lawrance Shepstone <lawrance@shepstone.org>
2013-02-03 16:07:52 +0000

    Completed Git class and tests.

--- src/PhpGit/Git.php
+++ src/PhpGit/Git.php
@@ -5,45 +5,110 @@ namespace PhpGit;
 use PhpProc\Process;
 use PhpGit\Exception\RuntimeException;
 
+/**
+ * Git
+ *
+ * Simple wrapper class for the Git binary.
+ */
 class Git
 {
+    /**
+     * Default Repository class.
+     */
+    const REPO_CLASS = '\PhpGit\Repository';
+
+    /**
+     * Path to the Git binary.
+     *
+     * @var string
+     */
     protected $path;
 
+    /**
+     * Process instance to be used to execute Git commands.
+     *
+     * @var \PhpProc\Process
+     */
     protected $process;
 
-    protected $repositoryClass = '\PhpGit\Repository';
+    /**
+     * Class to be used for repository instances.
+     *
+     * @var string
+     */
+    protected $repositoryClass = self::REPO_CLASS;
 
+    /**
+     * Constructs a new instance.
+     *
+     * @param string $path Path to the Git binary
+     * @param \PhpProc\Process $process Process instance to be used to execute Git commands.
+     */
     public function __construct($path, Process $process)
     {
         $this->path = (string) $path;
         $this->process = $process;
     }
 
+    /**
+     * Gets the path to the Git binary.
+     *
+     * @return string
+     */
     public function getPath()
     {
         return $this->path;
     }
 
+    /**
+     * Gets the process instance to be used to execute Git commands.
+     *
+     * @return \PhpProc\Process
+     */
     public function getProcess()
     {
         return $this->process;
     }
 
+    /**
+     * Gets the class to be used for repository instances.
+     *
+     * @return string
+     */
     public function getRepositoryClass()
     {
         return $this->repositoryClass;
     }
 
+    /**
+     * Sets the class to be used for repository instances.
+     *
+     * @param string $class Class to be used
+     * @return Git Fluent interface
+     * @throws Exception\RuntimeException
+     */
     public function setRepositoryClass($class)
     {
+        if (false === is_a($class, self::REPO_CLASS, true)) {
+            throw new RuntimeException("{$class} is not a valid repository class");
+        }
+
         $this->repositoryClass = (string) $class;
 
         return $this;
     }
 
-    public function __call($method, $arguments)
+    /**
+     * Method overloading to support calling Git commands directly on an instance.
+     *
+     * @param string $command Git command to call
+     * @param array $arguments Arguments to pass to the method supporting the command
+     * @return mixed Returns the result of the method supporting the Git command
+     * @throws Exception\RuntimeException
+     */
+    public function __call($command, $arguments)
     {
-        switch($method) {
+        switch($command) {
             case 'clone':
                 $method = 'invokeClone';
                 break;
@@ -52,26 +117,32 @@ class Git
                 $method = 'invokeOpen';
                 break;
 
-            case 'init':
-                $method = 'invokeInit';
-                break;
-
             default:
-                throw new RuntimeException("'git {$method}' is not supported");
+                throw new RuntimeException("'git {$command}' is not supported");
         }
 
         return call_user_func_array(array($this, $method), $arguments);
     }
 
+    /**
+     * Opens an existing repository at the specified file path.
+     *
+     * @param $path File path of the repository to open
+     * @return Repository
+     */
     protected function invokeOpen($path)
     {
-        if (false === is_a($this->repositoryClass, '\PhpGit\Repository', true)) {
-            throw new RuntimeException("{$this->repositoryClass} is not a valid repository class");
-        }
-
         return new $this->repositoryClass($path, $this);
     }
 
+    /**
+     * Clones a remote repository to the specified file path.
+     *
+     * @param string $url URL to remote repository to clone
+     * @param string $path File path to clone the repository to
+     * @return Repository
+     * @throws Exception\RuntimeException
+     */
     protected function invokeClone($url, $path)
     {
         $result = $this->process
@@ -79,8 +150,7 @@ class Git
             ->execute();
 
         if ($result->hasErrors()) {
-            throw new RuntimeException("Failed to clone {$url}; git says: "
-                . PHP_EOL . '  ' . $result->getStdErrContents());
+            throw new RuntimeException("Failed to clone {$url}: {$result->getStdErrContents()}");
         }
 
         return $this->invokeOpen($path);
--- tests/unit/PhpGit/GitTest.php
+++ tests/unit/PhpGit/GitTest.php
@@ -5,6 +5,14 @@ namespace PhpGit;
 use PhpGit\Git;
 use PhpProc\Process;
 
+class GitTest_Repository extends \PhpGit\Repository
+{
+    public function __construct()
+    {
+
+    }
+}
+
 class GitTest extends \PHPUnit_Framework_TestCase
 {
     public function testConstructSetsPathAndProcessInstanceCorrectly()
@@ -20,7 +28,7 @@ class GitTest extends \PHPUnit_Framework_TestCase
 
     public function testGetAndSetRepositoryClass()
     {
-        $class = '\PhpGit\Repository\Custom';
+        $class = '\PhpGit\GitTest_Repository';
 
         $git = new Git('/usr/bin/git', new Process());
 
@@ -29,11 +37,33 @@ class GitTest extends \PHPUnit_Framework_TestCase
         $this->assertSame($class, $git->getRepositoryClass());
     }
 
-    public function testCloneCallsTheGitCommandCorrectly()
+    public function testGetAndSetRepositoryClassWithInvalidClassThrowsException()
+    {
+        $class = '\PhpGit\Repository\Custom';
+
+        $git = new Git('/usr/bin/git', new Process());
+
+        $this->assertSame('\PhpGit\Repository', $git->getRepositoryClass());
+
+        $this->setExpectedException('\PhpGit\Exception\RuntimeException');
+
+        $this->assertSame($git, $git->setRepositoryClass($class));
+    }
+
+    public function testCloneCallsTheGitCommandCorrectlyAndReturnsRepositoryInstance()
     {
         $binary = '/usr/bin/git';
         $url = 'https://github.com/lshepstone/php-git.git';
         $path = '/path/to/repo';
+        $class = '\PhpGit\GitTest_Repository';
+
+        $result = $this->getMockBuilder('\PhpProc\Result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result->expects($this->once())
+            ->method('hasErrors')
+            ->will($this->returnValue(false));
 
         $process = $this->getMockBuilder('\PhpProc\Process')
             ->disableOriginalConstructor()
@@ -45,12 +75,64 @@ class GitTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnSelf());
 
         $process->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue($result));
+
+        $git = new Git($binary, $process);
+        $git->setRepositoryClass($class);
+
+        $repository = $git->clone($url, $path);
+
+        $this->assertInstanceOf($class, $repository);
+    }
+
+    public function testCloneWithGitCommandErrorsThrowsException()
+    {
+        $binary = '/usr/bin/git';
+        $url = 'https://github.com/lshepstone/php-git.git';
+        $path = '/path/to/repo';
+        $message = 'git encountered an error';
+
+        $result = $this->getMockBuilder('\PhpProc\Result')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $result->expects($this->once())
+            ->method('hasErrors')
+            ->will($this->returnValue(true));
+
+        $result->expects($this->once())
+            ->method('getStdErrContents')
+            ->will($this->returnValue($message));
+
+        $process = $this->getMockBuilder('\PhpProc\Process')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $process->expects($this->once())
             ->method('setCommand')
             ->with($this->equalTo("{$binary} clone \"{$url}\" \"{$path}\""))
             ->will($this->returnSelf());
 
+        $process->expects($this->once())
+            ->method('execute')
+            ->will($this->returnValue($result));
+
         $git = new Git($binary, $process);
 
+        $this->setExpectedException('\PhpGit\Exception\RuntimeException', "Failed to clone {$url}: {$message}");
+
         $git->clone($url, $path);
+    }
+
+    public function testCallUnsupportedCommandThrowsException()
+    {
+        $command = 'invalidCommand';
+
+        $git = new Git('/usr/bin/git', new Process());
+
+        $this->setExpectedException('\PhpGit\Exception\RuntimeException', "'git {$command}' is not supported");
+
+        $git->$command();
     }
 }
